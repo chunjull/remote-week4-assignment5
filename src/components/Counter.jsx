d68af26e34d088585814d21a8cb74442f5c1be15
chunjull <chunjuchein@gmail.com>
2024-07-12 08:58:01 +0800

    feat: first files

---
+++ src/components/Counter.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+function SingleCounter({ count, increment }) {
+  return (
+  <>
+  <div className="counterContainer">
+    <button onClick={ increment }>+1</button>
+    <div className="Number">{ count }</div>
+  </div>
+  </>
+  );
+}
+
+function Counter() {
+  const [counts, setCounts] = useState([0, 0, 0]);
+  const [allCount, setAllCount] = useState(0);
+
+  const incrementAll = () => {
+    setCounts(counts.map(count => count + 1));
+    setAllCount(allCount + 1);
+  }
+
+  const addCounter = () => {
+    setCounts([...counts, 0]);
+  }
+
+  const incrementSingle = (index) => {
+    const newCounts = [...counts];
+    newCounts[index] += 1;
+    setCounts(newCounts);
+  }
+
+  return(
+    <>
+    <div className="counterContainer">
+      <button onClick={ incrementAll }>All + 1</button>
+    </div>
+    {counts.map((count, index) => (
+      <SingleCounter 
+        key={index} 
+        count={count} 
+        increment={() => incrementSingle(index)} 
+      />
+    ))}
+    <div className="counterContainer">
+      <button onClick={addCounter}>Add a Counter</button>
+    </div>
+    </>
+  );
+}
+
+export default Counter;
